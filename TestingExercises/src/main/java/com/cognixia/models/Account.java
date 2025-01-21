21cb05f77c4ee98a1dcdf6e56604621b5332d94f
Mendi03 <salva.mendis.03@gmail.com>
2025-01-21 16:00:05 -0600

    completed first test, working on test2

--- TestingExercises/src/main/java/com/cognixia/models/Account.java
+++ TestingExercises/src/main/java/com/cognixia/models/Account.java
@@ -5,11 +5,43 @@ import java.util.List;
 
 public class Account {
     private int balance;
-    private List<List> allClaims = new ArrayList<>();
     private int id;
+    private List<List<String>> allClaims = new ArrayList<>();
+    private static int idCounter = 0;
+
 
     public Account() {
         this.balance = 0;
+        this.id = createID();
+    }
+
+    public int createID(){
+        return idCounter++;
+    }
+
+    public void addClaim(List<String> claim){
+        this.allClaims.add(claim);
+    }
+
+    public List<List<String>> getClaims(){
+        return this.allClaims;
+    }
+
+    public int getNOofClaims(){
+        return getClaims().size();
+    }
+
+    public void viewClaims(){
+        int claimsSize = getNOofClaims();
+        for(int i=0; i < claimsSize; i++){
+            System.out.println(this.allClaims.get(i));
+        }
+    }
+
+    public void sortClaims(){
+        //check if dates are valid
+        //find out how to compare dates with each other
+        //start sorting from newest to oldest
     }
 
     public int getBalance() {
--- TestingExercises/src/test/java/com/cognixia/models/AccountTest.java
+++ TestingExercises/src/test/java/com/cognixia/models/AccountTest.java
@@ -2,26 +2,29 @@ package com.cognixia.models;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Array;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AccountTest {
-  //1. As a member, I should be able to view a list of my past claims so I can verify my claim status
+  //1. As a member(user), I should be able to view a list of my past claims so I can verify my claim status
     @Test
     @DisplayName("Testing the viewClaims method with no parameters.")
-    void testViewClaims() {
-        List<List> claims = new ArrayList<>();
+    void testGetClaims() {
+        List<List<String>> claims = new ArrayList<>();
         List<String> claim1 = new ArrayList<>();
         List<String> claim2 = new ArrayList<>();
         List<String> claim3 = new ArrayList<>();
 
-        claim1.add("1000");
+
         claim1.add("01-17-2025");
-        claim2.add("2000");
+        claim1.add("2000");
         claim2.add("04-15-2024");
-        claim3.add("3000");
+        claim2.add("3000");
         claim3.add("06-09-2023");
+        claim3.add("1000");
 
         claims.add(claim1);
         claims.add(claim2);
@@ -32,14 +35,15 @@ public class AccountTest {
         accountTest.addClaim(claim2);
         accountTest.addClaim(claim3);
 
-        assertEquals(claims, accountTest.viewClaims());
+        assertEquals(claims, accountTest.getClaims());
+        accountTest.viewClaims();
     }
 
   //2. As a user, I should be able to search claims within a range of dates
   @Test
   @DisplayName("Testing the viewClaims method with parameters (dates).")
   void testViewClaimsP() {
-      List<List> claims = new ArrayList<>();
+      List<List<String>> claims = new ArrayList<>();
       List<String> claim1 = new ArrayList<>();
       List<String> claim2 = new ArrayList<>();
       List<String> claim3 = new ArrayList<>();
@@ -55,12 +59,14 @@ public class AccountTest {
       claims.add(claim2);
       claims.add(claim3);
 
+      //Collections.sort(claims);
+
       Account accountTest = new Account();
       accountTest.addClaim(claim1);
       accountTest.addClaim(claim2);
       accountTest.addClaim(claim3);
 
-      assertEquals(claims, accountTest.viewClaims("01-01-2024", "01-01-2026"));
+      //assertEquals(claims, accountTest.viewClaims("01-01-2024", "01-01-2026"));
   }
 }
   //3. As a provider, I should be able to upload several claims at once and add them to the database
